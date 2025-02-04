7825f6c079cdfe319a4e0a742c878ee65d34e956
sajjad-mirzapour <sajjad.mirzapour772gmail.com>
2025-02-04 10:51:21 +0330

    refactor : add fonts

---
+++ src/fonts/fonts.ts
@@ -0,0 +1,15 @@
+import localFont from "next/font/local";
+
+export const yekanFont = localFont({
+  src: [
+    {
+      path: "../fonts/Yekan.woff",
+    },
+    {
+      path: "../fonts/Yekan.ttf",
+    },
+    // {
+    //   path: "../fonts/Yekan.eot",
+    // },
+  ],
+});
