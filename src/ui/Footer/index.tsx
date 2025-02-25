e75dbeb92521e90117ab3f2989bc285e9b01f592
sajjad mirzapour <sajjad.mirzapour77@gmail.com>
2025-02-25 20:38:58 +0330

    feat(footer) :add related components

--- src/ui/Footer/index.tsx
+++ src/ui/Footer/index.tsx
@@ -1,13 +1,13 @@
-import AboutFooter from "./aboutFooter";
-import MenuFooter from "./menuFooter";
+import AboutFooter from "./about-footer";
+import MenuFooter from "./menu-footer";
 
 function Footer() {
-    return (
-        <footer className="xl:w-[88%] w-[92%] mx-auto">
-            <MenuFooter />
-            <AboutFooter />
-        </footer>
-    );
+  return (
+    <footer className="xl:w-[88%] w-[92%] mx-auto bg-white relative z-50">
+      <MenuFooter />
+      <AboutFooter />
+    </footer>
+  );
 }
 
 export default Footer;
