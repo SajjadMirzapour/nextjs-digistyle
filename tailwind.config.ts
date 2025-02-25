7e82ff5bf4c83efb60d9b543e870a5ccf668f7b5
sajjad mirzapour <sajjad.mirzapour77@gmail.com>
2025-02-25 20:55:07 +0330

    style :add tailwind to project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,29 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    // "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    // "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    // "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      screens: {
-        sm: "450px",
-      },
+    content: [
+        // "src/app/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        // "src/app/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+            screens: {
+                sm: "450px",
+                md: "769px",
+            },
+        },
+        keyframes: {
+            shimmer: {
+                "100%": {
+                    transform: "translateX(100%)",
+                },
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
